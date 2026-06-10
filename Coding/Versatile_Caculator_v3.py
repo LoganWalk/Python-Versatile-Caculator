df92822ea37d7eb4e7b7894a1bee916db38a0bdc
LoganWalk <loganwalk2010@gmail.com>
2026-06-10 11:17:35 -0500

    Updating calc

---
+++ Coding/Versatile_Caculator_v3.py
@@ -0,0 +1,55 @@
+answer = None
+
+
+
+def main():
+    section_selection()
+
+
+
+
+
+
+def section_selection():
+    print("What section are you looking for?")
+    print(f"1.Basic Math\n2.Advanced Math\n3.Geometery\n4.Algrebra 1\n5.Trigonomertey\n6.Calculus\n7.Algrebra 2")
+    answer = float(input("Enter the number next to the section you need: "))
+    answer_processing()
+
+def answer_processing():
+    if(answer == 1):
+        print("What caculation do you need to do?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
